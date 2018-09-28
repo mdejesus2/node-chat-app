0455e7a026b923bc4412b7ab6f861eeab7dee1ad
Michael De Jesus <michaeldejesus2@Michaels-MacBook-Pro-2.local>
2018-09-27 22:17:53 -0300

    web socket

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+
+ 
+});
+
+socket.on('newMessage', function(message){
+    console.log(message);
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
--- server/server.js
+++ server/server.js
@@ -1,14 +1,42 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New User connected'); 
+
+   
+    // socket.emit('newMessage', {
+    //     from: 'Michael',
+    //     text: 'recuerda enviarme los datos',
+    //     createdAt: 123333
+    // });
+    socket.on('createMessage', (message)=>{
+        console.log('Create Message:', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text
+          });
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected'); 
+    });
+});
+
+
+
+
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
 
